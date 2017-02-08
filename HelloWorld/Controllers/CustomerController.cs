be0d4b4386b8200de6dc92e1f754b0a729577ab7
Sagar <modi.sagar4u@gmail.com>
2017-02-08 17:47:13 +0530

    Created first Model and played around a little

---
+++ HelloWorld/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Load()
+        {
+            Customer cust =
+                new Customer
+                {
+                    CustomerCode = "1001",
+                    CustomerName = "Sagar"
+                };
+
+            return View("Customer",cust);
+        }
+
+        public ActionResult Add()
+        {
+            return View("AddCustomer");
+        }
+        public ActionResult Submit(Customer cust)
+        {
+            //Customer cust = new Customer();
+            //cust.CustomerCode = Request.Form["CustomerCode"];
+            //cust.CustomerName = Request.Form["CustomerName"];
+            return View("Customer",cust);
+        }
+    }
+}
